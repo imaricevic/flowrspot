1a63884d33dd5c0a265594e7254a88cbc21b9cab
imaricevic <ivan.maricevic5@gmail.com>
2020-09-22 18:44:17 +0200

    Initial Commit: Raw SpringBoot project with dependencies.

---
+++ src/main/java/povio/flowrspot/FlowrspotApplication.java
@@ -0,0 +1,13 @@
+package povio.flowrspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowrspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowrspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/povio/flowrspot/FlowrspotApplicationTests.java
@@ -0,0 +1,13 @@
+package povio.flowrspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowrspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
