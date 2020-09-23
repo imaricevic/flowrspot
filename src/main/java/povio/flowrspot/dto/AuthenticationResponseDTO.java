67e3fc0a8341d8d9183ebba69493feea5a9985e9
imaricevic <ivan.maricevic5@gmail.com>
2020-09-23 19:33:58 +0200

    Implemented a JWT authentication service

---
+++ src/main/java/povio/flowrspot/dto/AuthenticationResponseDTO.java
@@ -0,0 +1,13 @@
+package povio.flowrspot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponseDTO {
+    private String jwt;
+}
+
